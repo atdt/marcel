be6eeb1615878e9240ccfbdef9904a6012e180e8
Ori Livneh <ori.livneh@gmail.com>
2011-10-16 02:59:13 -0400

    ep.io-related tweaks

--- marcel.py
+++ marcel.py
@@ -1,14 +1,17 @@
+from redis import Redis
 from flask import Flask, render_template, flash
-from entries import redis, offers, requests
 
 # config
 DEBUG = True
 SECRET_KEY = 'development key'
+REDIS = {}  # passed as kwargs to redis.Redis()
 
 # init app
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('MARCEL_SETTINGS', silent=True)
+
+redis = Redis(**app.config['REDIS'])
 
 # utils
 def reset():
@@ -26,6 +29,46 @@ def add_dummy_data():
     requests.upvote(uid)
     requests.upvote(uid)
 
+# models
+class EntryManager(object):
+    def __init__(self, type):
+        self.type = type
+
+    def get(self, uid):
+        item = redis.hgetall("marcel:%s:%s" % (self.type, uid))
+        item['tags'] = redis.smembers("marcel:%s:%s:tags" % (self.type, uid))
+        item['type'] = self.type
+        return item
+
+    def all(self):
+        keys = redis.zrevrange(
+            name="marcel:%s" % self.type,
+            start=0,
+            num=-1,
+            withscores=True,
+            score_cast_func=int
+        )
+        items = []
+        for uid, score in keys:
+            item = self.get(uid)
+            item['score'] = score  # annotate with score
+            items.append(item)
+        return items
+
+    def add(self, **mapping):
+        uid = redis.incr("marcel:%s:next_uid" % self.type)
+        redis.zadd("marcel:%s" % self.type, uid, 0)
+        tags = mapping.pop('tags')
+        redis.sadd("marcel:%s:%s:tags" % (self.type, uid), *tags)
+        redis.hmset("marcel:%s:%s" % (self.type, uid), mapping)
+        return uid
+
+    def upvote(self, uid):
+        return redis.zincrby("marcel:%s" % self.type, uid, 1)
+
+
+requests = EntryManager("request")
+offers = EntryManager("offer")
 # views
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -68,6 +111,9 @@ def add_entry():
     flash("Request successfully posted")
     pass
 
+@app.route('/redis_debug')
+def redis_debug():
+    return render_template('redis_debug.html', info=redis.info())
 
 if __name__ == "__main__":
     app.run()
